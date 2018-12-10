6fbcd2c34d4c634ae43d909c7fdd94330f097ef5
Andrey <79040106940@yandex.ru>
2018-12-10 23:23:33 +0300

    files from folder

--- label_image.py
+++ label_image.py
@@ -1,18 +1,3 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -20,9 +5,13 @@ from __future__ import print_function
 import argparse
 import sys
 import time
+import os
 
 import numpy as np
 import tensorflow as tf
+
+
+FOLDER='cxr/'
 
 def load_graph(model_file):
   graph = tf.Graph()
@@ -87,8 +76,8 @@ def main(file_name):
 
     input_name = "import/" + input_layer
     output_name = "import/" + output_layer
-    input_operation = graph.get_operation_by_name(input_name);
-    output_operation = graph.get_operation_by_name(output_name);
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
 
     with tf.Session(graph=graph) as sess:
       start = time.time()
@@ -100,15 +89,13 @@ def main(file_name):
     top_k = results.argsort()[-5:][::-1]
     labels = load_labels(label_file)
 
-    print(file_name, labels[results.argmax()], results.max())
-    print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
+    print()
+    print(file_name.replace(FOLDER, ''), labels[results.argmax()], results.max())
     template = "{} (score={:0.5f})"
     for i in top_k:
       print(template.format(labels[i], results[i]))
 
 
 if __name__ == '__main__':
-    files = ['norm1.jpg', 'tbc1.jpg']
-    for file in files:
-        main(file)
-# todo files from folder
+    
+    [main(FOLDER + f) for f in os.listdir(FOLDER)]
